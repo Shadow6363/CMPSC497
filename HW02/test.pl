bfe7fe977a944337ac4ae5e1c083ea8322fad1c5
Christopher Cope <Shadow6363@gmail.com>
2013-10-09 15:56:06 -0400

    More work on tracing the path.

--- HW02/test.pl
+++ HW02/test.pl
@@ -11,10 +11,13 @@ sub min_edit_distance {
     my @ar2 = split //, $source;
 
     my @distances;
-    $distances[$_][0] = $_ foreach (0 .. @ar1);
-    $distances[0][$_] = $_ foreach (0 .. @ar2);
+    $distances[$_][0][0] = $_ foreach (0 .. @ar1);
+    $distances[0][$_][0] = $_ foreach (0 .. @ar2);
+    $distances[$_][0][1] = "L" foreach (0 .. @ar1);
+    $distances[0][$_][1] = "D" foreach (0 .. @ar2);
+    $distances[0][0][1] = "C";
 
-    my @direction = ("A", "B", "C");
+    my $direction = "";
     my $distance;
     my $L;
     my $D;
@@ -24,26 +27,43 @@ sub min_edit_distance {
         foreach my $j (1 .. @ar2){
             my $cost = $ar1[$i - 1] eq $ar2[$j - 1] ? 0 : 2;
 
-            $L = $distances[$i - 1][$j] + 1;
-            $D = $distances[$i][$j - 1] + 1;
-            $C = $distances[$i - 1][$j - 1] + $cost;
+            $L = $distances[$i - 1][$j][0] + 1;
+            $D = $distances[$i][$j - 1][0] + 1;
+            $C = $distances[$i - 1][$j - 1][0] + $cost;
             $distance = min($L, $D, $C);
 
             if ($distance == $L) {
-                push @direction, "L";
+                $direction = "L";
             } elsif ($distance == $D) {
-                push @direction, "D";
+                $direction = "D";
             } else {
-                push @direction, "C";
+                $direction = "C";
             }
 
-            $distances[$i][$j] = $distance;
+            $distances[$i][$j][0] = $distance;
+            $distances[$i][$j][1] = $direction;
         }
     }
 
-    @bleh = ($distances[@ar1][@ar2], @direction);
 
-    return @bleh;
+    $i = @ar1;
+    $j = @ar2;
+    $direction = "";
+    while ($i > 0 and $j > 0) {
+        if($distances[$i][$j][1] eq "D") {
+            $direction = $direction . "D";
+            $j = $j - 1;
+        } elsif($distances[$i][$j][1] eq "L") {
+            $direction = $direction . "L";
+            $i = $i - 1;
+        } else {
+            $direction = $direction . "C";
+            $i = $i - 1;
+            $j = $j - 1;
+        }
+    }
+
+    return ($distances[@ar1][@ar2][0], scalar reverse $direction);
 }
 
 my %dictionary;
@@ -73,20 +93,19 @@ while (my $word = <$fh>) {
             "2" => 9999
         );
         my %routes = (
-            "0" => qw/0/,
-            "1" => qw/1/,
-            "2" => qw/2/
+            "0" => "",
+            "1" => "",
+            "2" => ""
         );
 
 
         foreach my $key (keys %dictionary) {
             if (abs(length($key) - length($word)) <= $top_word_distances{$top_words[2]}) {
                 @route = min_edit_distance($key, $word);
-                #print @route;
                 if ($route[0] < $top_word_distances{$top_words[2]}) {
                     delete $routes{$top_words[2]};
                     delete $top_word_distances{$top_words[2]};
-                    $routes{$key} = @route[1, -1];
+                    $routes{$key} = $route[1];
                     $top_word_distances{$key} = $route[0];
                     @top_words = (sort { $top_word_distances{$a} <=> $top_word_distances{$b} } keys %top_word_distances);
                 }
@@ -96,7 +115,7 @@ while (my $word = <$fh>) {
         print "Misspelled: $word; Suggested: ";
         print %top_word_distances;
         print ", ";
-        print $routes{"fated"};
+        print %routes;
         print"\n";
     }
 }
